a43bcfbf90061d77989fe22964864f839b0adebe
Connor Wyatt <connorwyatt1@gmail.com>
2024-12-13 14:47:36 +0000

    Update nvim config

---
+++ .config/nvim/after/plugin/transparent.lua
@@ -0,0 +1,3 @@
+require("transparent").clear_prefix("NeoTree")
+require("transparent").clear_prefix("lualine_c")
+require("transparent").clear_prefix("lualine_x")
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -18,6 +18,51 @@ return {
         version = false,
         build = ":TSUpdate",
     },
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        opts = {
+            bigfile = { enabled = true },
+            dim = { enabled = true },
+            indent = { enabled = true },
+            input = { enabled = true },
+            lazygit = { enabled = true },
+            notifier = {
+                enabled = true,
+                timeout = 3000,
+                wrap = true,
+            },
+            quickfile = { enabled = true },
+            scroll = { enabled = true },
+        },
+        keys = {
+            { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+            { "<leader>gl", function() Snacks.lazygit() end,               desc = "Lazygit" },
+            { "<c-/>",      function() Snacks.terminal() end,              desc = "Toggle Terminal" },
+        },
+        init = function()
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "VeryLazy",
+                callback = function()
+                    -- Setup some globals for debugging (lazy-loaded)
+                    _G.dd = function(...)
+                        Snacks.debug.inspect(...)
+                    end
+                    _G.bt = function()
+                        Snacks.debug.backtrace()
+                    end
+                    vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+                    Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+                    Snacks.toggle.diagnostics():map("<leader>ud")
+                    Snacks.toggle.inlay_hints():map("<leader>uh")
+                    Snacks.toggle.indent():map("<leader>ug")
+                    Snacks.toggle.dim():map("<leader>uD")
+                end,
+            })
+        end,
+    },
     { "nvim-treesitter/nvim-treesitter-context" },
     { "windwp/nvim-ts-autotag" },
     {
@@ -93,6 +138,7 @@ return {
         event = "InsertEnter",
         config = true,
     },
+    { "xiyaowong/transparent.nvim" },
 
     -- themes
     { "rose-pine/neovim" },
