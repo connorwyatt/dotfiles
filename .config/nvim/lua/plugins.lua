9cba1949f84bec1ddcd90399bbc742d758e35368
Connor Wyatt <connorwyatt1@gmail.com>
2024-12-14 22:13:32 +0000

    Update nvim config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -85,18 +85,7 @@ return {
         event = "VeryLazy",
     },
     { "tpope/vim-fugitive" },
-    {
-        "lewis6991/gitsigns.nvim",
-        opts = {
-            signs = {
-                add = { text = "+" },
-                change = { text = "~" },
-                delete = { text = "_" },
-                topdelete = { text = "‾" },
-                changedelete = { text = "~" },
-            }
-        },
-    },
+    { "lewis6991/gitsigns.nvim" },
     { "pwntester/octo.nvim" },
     {
         "nvim-neo-tree/neo-tree.nvim",
