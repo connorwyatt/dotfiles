972745285c7d754d17dd6a1cc1bafcfe2afcca9f
Connor Wyatt <connorwyatt1@gmail.com>
2025-01-06 14:12:26 +0000

    Update nvim

--- .config/nvim/after/plugin/transparent.lua
+++ .config/nvim/after/plugin/transparent.lua
@@ -1,3 +1,3 @@
-require("transparent").clear_prefix("NeoTree")
-require("transparent").clear_prefix("lualine_c")
-require("transparent").clear_prefix("lualine_x")
+-- require("transparent").clear_prefix("NeoTree")
+-- require("transparent").clear_prefix("lualine_c")
+-- require("transparent").clear_prefix("lualine_x")
