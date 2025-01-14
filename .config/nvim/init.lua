3d09fc185dd7e3bb3797c433fb10d27a35a71c06
Connor Wyatt <connorwyatt1@gmail.com>
2025-01-14 09:31:34 +0000

    Initial reworking of nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -282,11 +282,11 @@ require("lazy").setup({
                 update_interval = 1000,
                 set_dark_mode = function()
                     vim.api.nvim_set_option_value("background", "dark", {})
-                    vim.cmd("colorscheme sakura-night")
+                    vim.cmd("colorscheme rose-pine-moon")
                 end,
                 set_light_mode = function()
                     vim.api.nvim_set_option_value("background", "light", {})
-                    vim.cmd("colorscheme sakura-night")
+                    vim.cmd("colorscheme rose-pine-dawn")
                 end,
             }
         },
