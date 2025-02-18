10a2d4c107ae21c7ccc2bd2e0141d988e4f63c1e
Connor Wyatt <connorwyatt1@gmail.com>
2025-02-18 23:41:01 +0000

    feat(nvim): make background transparent

--- .config/nvim/lua/plugins/theming.lua
+++ .config/nvim/lua/plugins/theming.lua
@@ -2,4 +2,18 @@ return {
     {
         "connorwyatt/sakura.nvim",
     },
+    {
+        "xiyaowong/transparent.nvim",
+        opts = {
+            exclude_groups = {
+                "CursorLine",
+                "CursorLineNr",
+            },
+            extra_groups = {
+                "NeoTree",
+                "lualine_c",
+                "lualine_x",
+            },
+        },
+    },
 }
