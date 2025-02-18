282c3d348d67dbc0eeec42088dcd10a0367e4b59
Connor Wyatt <connorwyatt1@gmail.com>
2025-02-18 22:12:31 +0000

    feat(nvim): ensure LSP semantic tokens do not override treesitter highlights

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,7 @@
 local configs = require("nvim-treesitter.configs")
 
+vim.highlight.priorities.semantic_tokens = 95 -- Or any number lower than 100, treesitter's priority level
+
 configs.setup({
     auto_install = true,
     ensure_installed = { "comment", },
