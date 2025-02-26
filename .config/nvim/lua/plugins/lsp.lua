72be4d8a7907ece3d774a96a878cdaa0085a0dab
Connor Wyatt <connorwyatt1@gmail.com>
2025-02-26 14:35:45 +0000

    feat(nvim): tweaks to blink completion

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -21,16 +21,18 @@ return {
         opts = {
             keymap = {
                 preset = "enter",
+                ["<Esc>"] = { "hide", "fallback", },
                 ["<C-s>"] = { function(cmp) cmp.show({ providers = { "snippets" } }) end },
+                ["<Tab>"] = { "select_and_accept", "snippet_forward", "fallback" },
             },
             appearance = {
-                use_nvim_cmp_as_default = true,
-                nerd_font_variant = "mono"
+                nerd_font_variant = "normal"
             },
             completion = {
                 documentation = {
                     auto_show = true,
                     auto_show_delay_ms = 0,
+                    update_delay_ms = 0,
                 },
                 list = {
                     selection = {
@@ -38,8 +40,15 @@ return {
                         auto_insert = true,
                     },
                 },
+                menu = {
+                    draw = {
+                        treesitter = { "lsp" },
+                    },
+                },
                 ghost_text = {
-                    enabled = false,
+                    enabled = true,
+                    show_with_selection = true,
+                    show_without_selection = true,
                 },
             },
             signature = {
