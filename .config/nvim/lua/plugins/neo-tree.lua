69e699ffe7532c23dfc9cf079c01381603fe58ea
Connor Wyatt <connor@wealthkernel.com>
2025-03-03 12:46:35 +0000

    feat(nvim): configure neo tree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -11,10 +11,41 @@ return {
         window = {
             position = "current",
         },
+        search_limit = 1000,
         follow_current_file = {
             enabled = true,
             leave_dirs_open = false,
         },
         group_empty_dirs = true,
+        use_default_mappings = false,
+        filesystem = {
+            scan_mode = "deep",
+            window = {
+                mappings = {
+                    ["<cr>"] = { "open", desc = "Open", config = { expand_nested_files = true, }, },
+                    ["<bs>"] = { "navigate_up", desc = "Navigate up", },
+                    ["."] = { "set_root", desc = "Set root", },
+                    ["a"] = { "add", desc = "Add", config = { show_path = "relative", }, },
+                    ["b"] = { "rename_basename", desc = "Rename basename", },
+                    ["c"] = { "close_all_nodes", desc = "Close all nodes", },
+                    ["d"] = { "delete", desc = "Delete", },
+                    ["s"] = { "filter_as_you_type", desc = "Search", },
+                    ["p"] = { "paste_from_clipboard", desc = "Paste from clipboard", },
+                    ["r"] = { "rename", desc = "Rename", },
+                    ["x"] = { "cut_to_clipboard", desc = "Cut to clipboard", },
+                    ["y"] = { "copy_to_clipboard", desc = "Copy to clipboard", },
+                },
+            },
+        },
+        git_status = {
+            window = {
+                mappings = {
+                    ["<cr>"] = { "open", desc = "Open", config = { expand_nested_files = true, }, },
+                    ["b"] = { "rename_basename", desc = "Rename basename", },
+                    ["s"] = { "filter_as_you_type", desc = "Search", },
+                    ["r"] = { "rename", desc = "Rename", },
+                },
+            },
+        },
     },
 }
