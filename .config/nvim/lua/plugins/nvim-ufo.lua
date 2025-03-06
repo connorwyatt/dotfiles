80d32e5a058a0f457cfbec4598729eb3e82fa6fd
Connor Wyatt <connor@wealthkernel.com>
2025-03-06 23:08:45 +0000

    feat(nvim): replace folded text

--- .config/nvim/lua/plugins/nvim-ufo.lua
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -6,14 +6,14 @@ return {
         },
         opts = {
             close_fold_kinds_for_ft = {
-                default = { 'imports', 'comment' },
+                default = { "imports", "comment" },
             },
             provider_selector = function()
-                return { 'treesitter', 'indent', }
+                return { "treesitter", "indent", }
             end,
             fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
                 local newVirtText = {}
-                local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+                local suffix = ("  %d lines "):format(endLnum - lnum)
                 local sufWidth = vim.fn.strdisplaywidth(suffix)
                 local targetWidth = width - sufWidth
                 local curWidth = 0
@@ -29,13 +29,13 @@ return {
                         chunkWidth = vim.fn.strdisplaywidth(chunkText)
                         -- str width returned from truncate() may less than 2nd argument, need padding
                         if curWidth + chunkWidth < targetWidth then
-                            suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+                            suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
                         end
                         break
                     end
                     curWidth = curWidth + chunkWidth
                 end
-                table.insert(newVirtText, { suffix, 'MoreMsg' })
+                table.insert(newVirtText, { suffix, "MoreMsg" })
                 return newVirtText
             end,
         },
