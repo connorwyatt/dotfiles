b534ab645961eeec412d46df887ba36004c7a403
Connor Wyatt <connorwyatt1@gmail.com>
2025-03-07 23:36:58 +0000

    feat(zsh): ensure sources-local directory exists

--- .config/zsh/init.sh
+++ .config/zsh/init.sh
@@ -8,14 +8,16 @@ path+=("$HOME/.bin-local")
 autoload -Uz compinit
 compinit
 
-SOURCES_FILES_STR=$(fd --glob '*.sh' ~/.config/zsh/sources)
+SOURCES_FILES_STR=$(fd --glob '*.sh' $HOME/.config/zsh/sources)
 SOURCES_FILES=($(echo $SOURCES_FILES_STR | tr '\n' ' '))
 
 for FILE in $SOURCES_FILES; do
     source $FILE
 done
 
-SOURCES_LOCAL_FILES_STR=$(fd --glob '*.sh' ~/.config/zsh/sources-local)
+mkdir -p $HOME/.config/zsh/sources-local
+
+SOURCES_LOCAL_FILES_STR=$(fd --glob '*.sh' $HOME/.config/zsh/sources-local)
 SOURCES_LOCAL_FILES=($(echo $SOURCES_LOCAL_FILES_STR | tr '\n' ' '))
 
 for FILE in $SOURCES_LOCAL_FILES; do
