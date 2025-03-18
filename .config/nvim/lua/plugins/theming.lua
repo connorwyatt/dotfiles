1932cea7a2f8ae402a666a37ac9d71183602fdc2
Connor Wyatt <connor@wealthkernel.com>
2025-03-18 11:27:23 +0000

    feat(nvim,ghostty): enable transparency

--- .config/nvim/lua/plugins/theming.lua
+++ .config/nvim/lua/plugins/theming.lua
@@ -5,6 +5,10 @@ return {
         priority = 10000,
     },
     {
+        "xiyaowong/transparent.nvim",
+        config = true
+    },
+    {
         "f-person/auto-dark-mode.nvim",
         opts = {
             update_interval = 1000,
@@ -12,11 +16,13 @@ return {
                 local dark_mode_theme = "sakura-night"
                 vim.api.nvim_set_option_value("background", "dark", {})
                 vim.cmd("colorscheme " .. dark_mode_theme)
+                vim.cmd(":TransparentEnable")
             end,
             set_light_mode = function()
                 local light_mode_theme = "sakura-day"
                 vim.api.nvim_set_option_value("background", "light", {})
                 vim.cmd("colorscheme " .. light_mode_theme)
+                vim.cmd(":TransparentEnable")
             end,
         },
     },
