07fce173c2e19a37c953458ab18ba77aa8800ac4
Connor Wyatt <connorwyatt1@gmail.com>
2025-04-02 00:23:29 +0100

    feat(nvim): improve telescope setup

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local telescope = require("telescope")
+local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
 local whichKey = require("which-key")
 
@@ -10,14 +11,14 @@ local dropdown = {
     layout_config = {
         prompt_position = "top",
         anchor = "N",
-        preview_cutoff = 1,
+        preview_cutoff = 20,
 
         width = function(_, max_columns, _)
             return math.min(max_columns, 120)
         end,
 
         height = function(_, _, max_lines)
-            return math.min(max_lines, 15)
+            return math.min(max_lines, 20)
         end,
     },
     border = true,
@@ -28,24 +29,28 @@ local dropdown = {
     },
 }
 
-local ivy = {
+local cursor = {
+    theme = "cursor",
     sorting_strategy = "ascending",
-
-    layout_strategy = "bottom_pane",
+    results_title = false,
+    layout_strategy = "cursor",
     layout_config = {
-        height = 25,
+        width = function(_, max_columns, _)
+            return math.min(max_columns, 80)
+        end,
+        height = function(_, _, max_lines)
+            return math.min(max_lines, 9)
+        end,
     },
-
-    border = true,
     borderchars = {
-        prompt = { "─", " ", " ", " ", "─", "─", " ", " " },
-        results = { " " },
+        prompt = { "─", "│", " ", "│", "┌", "┐", "│", "│" },
+        results = { "─", "│", "─", "│", "├", "┤", "┘", "└" },
         preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
     },
 }
 
 telescope.setup({
-    defaults = vim.tbl_extend("keep", {
+    defaults = {
         vimgrep_arguments = {
             "rg",
             "--color=never",
@@ -56,7 +61,44 @@ telescope.setup({
             "--smart-case",
             "--trim",
         },
-    }, dropdown),
+        mappings = {
+            i = {
+                ["<esc>"] = actions.close,
+            },
+        },
+    },
+    pickers = {
+        buffers = vim.tbl_extend(
+            "force",
+            { mappings = { i = { ["<C-d>"] = actions.delete_buffer + actions.move_to_top } } },
+            dropdown
+        ),
+        command_history = dropdown,
+        commands = dropdown,
+        current_buffer_fuzzy_find = dropdown,
+        find_files = dropdown,
+        git_bcommits = dropdown,
+        git_bcommits_range = dropdown,
+        git_branches = dropdown,
+        git_commits = dropdown,
+        git_files = dropdown,
+        git_status = dropdown,
+        git_stash = dropdown,
+        help_tags = dropdown,
+        highlights = dropdown,
+        live_grep = dropdown,
+        lsp_document_symbols = dropdown,
+        lsp_dynamic_workspace_symbols = dropdown,
+        marks = dropdown,
+        oldfiles = dropdown,
+        registers = dropdown,
+        resume = dropdown,
+        search_history = dropdown,
+        treesitter = dropdown,
+    },
+    extensions = {
+        ["ui-select"] = cursor,
+    },
 })
 
 telescope.load_extension("fzf")
@@ -73,9 +115,16 @@ whichKey.add({
     { "<leader>sS", builtin.lsp_dynamic_workspace_symbols, desc = "LSP workspace symbols" },
     { "<leader>sb", builtin.current_buffer_fuzzy_find, desc = "Current buffer" },
     { "<leader>sc", builtin.commands, desc = "Commands" },
-    { "<leader>sd", builtin.git_status, desc = "Git diff" },
     { "<leader>sf", builtin.find_files, desc = "Files" },
-    { "<leader>sg", builtin.git_files, desc = "Git files" },
+    { "<leader>s'", builtin.marks, desc = "Marks" },
+    { "<leader>sg", group = "Git" },
+    { mode = "v", "<leader>sgc", builtin.git_bcommits_range, desc = "Selection commits" },
+    { mode = "n", "<leader>sgc", builtin.git_bcommits, desc = "Buffer commits" },
+    { "<leader>sgC", builtin.git_commits, desc = "Commits" },
+    { "<leader>sgb", builtin.git_branches, desc = "Branches" },
+    { "<leader>sgd", builtin.git_status, desc = "Diff" },
+    { "<leader>sgf", builtin.git_files, desc = "Files" },
+    { "<leader>sgs", builtin.git_stash, desc = "Stashes" },
     { "<leader>sh", builtin.help_tags, desc = "Help" },
     { "<leader>sl", builtin.resume, desc = "Resume last search" },
     { "<leader>sr", builtin.oldfiles, desc = "Recent files" },
