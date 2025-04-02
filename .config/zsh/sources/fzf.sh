f5133274003ec328f15651f316d0f41bb04d9882
Connor Wyatt <connor@wealthkernel.com>
2025-04-02 15:24:59 +0100

    feat(fzf): update fzf config

--- .config/zsh/sources/fzf.sh
+++ .config/zsh/sources/fzf.sh
@@ -1,3 +1,5 @@
 export FZF_DEFAULT_OPTS_FILE="$HOME/.config/zsh/fzf-default-opts"
+export FZF_DEFAULT_COMMAND='fd --type file'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 source <(fzf --zsh)
