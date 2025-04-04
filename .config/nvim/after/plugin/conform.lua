2ac95378b5db85ab8bce08be3e221510467160de
Connor Wyatt <connor@wealthkernel.com>
2025-04-04 16:48:41 +0100

    fix(nvim): correct typo

--- .config/nvim/after/plugin/conform.lua
+++ .config/nvim/after/plugin/conform.lua
@@ -72,7 +72,7 @@ vim.api.nvim_create_user_command("ReformatToggle", function(args)
         -- ReformatToggle! will toggle formatting just for this buffer
         vim.b.disable_autoformat = not vim.b.disable_autoformat
     else
-        vim.g.disable_autoformat = not vim.g.disabled_autoformat
+        vim.g.disable_autoformat = not vim.g.disable_autoformat
     end
 end, {
     desc = "Toggle auto reformat on save",
