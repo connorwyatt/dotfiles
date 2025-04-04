c9067c7452a659f97cd1e7224c9821cd5000ef55
Connor Wyatt <connorwyatt1@gmail.com>
2025-04-04 08:12:33 +0100

    feat(nvim): remove trouble

--- .config/nvim/after/plugin/trouble.lua
+++
@@ -1,8 +0,0 @@
-local whichKey = require("which-key")
-
-whichKey.add({
-    { "<leader>db", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer diagnostics" },
-    { "<leader>dw", "<cmd>Trouble diagnostics toggle<cr>", desc = "Workspace diagnostics" },
-    { "<leader>lo", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols sidebar" },
-    { "<leader>ll", "<cmd>Trouble lsp open focus=true<cr>", desc = "LSP definitions & references" },
-})
