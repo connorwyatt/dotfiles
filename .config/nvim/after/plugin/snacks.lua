b4e58a0b20bbc5397b93df1b36b9d81b085a48a0
Connor Wyatt <connorwyatt1@gmail.com>
2025-04-04 08:47:11 +0100

    feat(nvim): replace refjump

--- .config/nvim/after/plugin/snacks.lua
+++ .config/nvim/after/plugin/snacks.lua
@@ -1,2 +1,18 @@
+local whichKey = require("which-key")
 
+whichKey.add({
+    {
+        "[w",
+        function()
+            Snacks.words.jump(-1)
+        end,
+        desc = "Previous word occurrence",
+    },
+    {
+        "]w",
+        function()
+            Snacks.words.jump(1)
+        end,
+        desc = "Next word occurrence",
+    },
 })
--- .config/nvim/lua/plugins/refjump.lua
+++
@@ -1,6 +0,0 @@
-return {
-    {
-        "mawkler/refjump.nvim",
-        config = true,
-    },
-}
