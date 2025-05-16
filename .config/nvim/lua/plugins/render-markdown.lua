77e9775cd919a4f30f28d2181f357bb35f73c6e3
Connor Wyatt <connor@wealthkernel.com>
2025-05-16 11:51:32 +0100

    feat(nvim): add some extra plugins for viewing diffs, markdown and autotags

---
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
+        },
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {
+            completions = {
+                blink = {
+                    enabled = true,
+                },
+            },
+        },
+    },
+}
