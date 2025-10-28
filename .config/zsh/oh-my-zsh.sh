0f402bd7e55faaeb5439c96c126929bf0534fadc
Connor Wyatt <connor@wealthkernel.com>
2025-10-28 13:06:24 +0000

    feat(starship): add starship

--- .config/zsh/oh-my-zsh.sh
+++ .config/zsh/oh-my-zsh.sh
@@ -4,8 +4,6 @@ if [[ ":$FPATH:" != *":/Users/connorwyatt/.zsh/completions:"* ]]; then export FP
 
 export ZSH="$HOME/.oh-my-zsh"
 
-ZSH_THEME="connor"
-
 zstyle ":omz:update" mode auto
 
 plugins=(zsh-autosuggestions)
