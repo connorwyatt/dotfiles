76aee52d89822d5b88a2b8b37da9b1a8d9a988fd
Connor Wyatt <connorwyatt1@gmail.com>
2025-11-12 13:36:12 +0000

    feat(zsh): fix nvm source

--- .config/zsh/sources/nvm.sh
+++ .config/zsh/sources/nvm.sh
@@ -1,4 +1,4 @@
-if (( ${+commands[nvm]} )); then
+if brew list --formula | grep -q "^nvm$"; then
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
