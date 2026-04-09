112f11ce2ba9fc5f9b3104e82a7edeae36c4db18
Connor Wyatt <connor.wyatt@alpaca.markets>
2026-04-09 14:29:07 +0100

    fix(zsh): ensure HOMEBREW_NO_ENV_HINTS variable is exported

--- .config/zsh/sources/homebrew.sh
+++ .config/zsh/sources/homebrew.sh
@@ -1,7 +1,7 @@
 if (( ${+commands[brew]} )); then
     eval "$(/opt/homebrew/bin/brew shellenv)"
 
-    HOMEBREW_NO_ENV_HINTS=1
+    export HOMEBREW_NO_ENV_HINTS=1
 
     UPDATE_CACHE="$HOME/.config/zsh/.brew_last_check"
     current_date=$(date +%Y%m%d)
