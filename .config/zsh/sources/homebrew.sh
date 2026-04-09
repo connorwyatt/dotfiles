13bc78a52b23770c34a1f78ac51eac7ae572fdbf
Connor Wyatt <connor.wyatt@alpaca.markets>
2026-04-09 09:19:39 +0100

    feat(zsh): show outdated brew packages in first terminal of the day

--- .config/zsh/sources/homebrew.sh
+++ .config/zsh/sources/homebrew.sh
@@ -1,3 +1,26 @@
 if (( ${+commands[brew]} )); then
     eval "$(/opt/homebrew/bin/brew shellenv)"
+
+    UPDATE_CACHE="$HOME/.config/zsh/.brew_last_check"
+    current_date=$(date +%Y%m%d)
+
+    if [[ ! -f "$UPDATE_CACHE" ]] || [[ "$(cat "$UPDATE_CACHE")" != "$current_date" ]]; then
+        echo ""
+        echo -n "Checking for Homebrew updates..."
+
+        data=$(brew outdated --json=v2)
+
+        formulae_list=$(echo "$data" | jq -r '[.formulae[].name] | join(", ")')
+        casks_list=$(echo "$data" | jq -r '[.casks[].name] | join(", ")')
+
+        if [[ -n "$formulae_list" || -n "$casks_list" ]]; then
+            echo -e "\r\e[KOutdated Homebrew packages:"
+            [[ -n "$formulae_list" ]] && echo -e "  \e[33m•\e[0m Formulae: $formulae_list"
+            [[ -n "$casks_list" ]] && echo -e "  \e[33m•\e[0m Casks: $casks_list"
+        else
+            echo -e "\r\e[K\e[32m✔\e[0m All Homebrew packages are up to date!"
+        fi
+
+        echo "$current_date" > "$UPDATE_CACHE"
+    fi
 fi
