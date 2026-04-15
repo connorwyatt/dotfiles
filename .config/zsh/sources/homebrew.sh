7c34bfa5fb557afce5b5dc180372e984edaa6003
Connor Wyatt <connor.wyatt@alpaca.markets>
2026-04-15 12:14:59 +0100

    feat(zsh): update homebrew logging when updating

--- .config/zsh/sources/homebrew.sh
+++ .config/zsh/sources/homebrew.sh
@@ -7,8 +7,7 @@ if (( ${+commands[brew]} )); then
     current_date=$(date +%Y%m%d)
 
     if [[ ! -f "$UPDATE_CACHE" ]] || [[ "$(cat "$UPDATE_CACHE")" != "$current_date" ]]; then
-        echo ""
-        echo -n "Checking for Homebrew updates..."
+        echo "\nChecking for Homebrew updates..."
 
         data=$(brew outdated --json=v2)
 
@@ -16,11 +15,11 @@ if (( ${+commands[brew]} )); then
         casks_list=$(echo "$data" | jq -r '[.casks[].name] | join(", ")')
 
         if [[ -n "$formulae_list" || -n "$casks_list" ]]; then
-            echo -e "\r\e[KOutdated Homebrew packages:"
+            echo -e "\e[KOutdated Homebrew packages:"
             [[ -n "$formulae_list" ]] && echo -e "  \e[33m•\e[0m Formulae: $formulae_list"
             [[ -n "$casks_list" ]] && echo -e "  \e[33m•\e[0m Casks: $casks_list"
         else
-            echo -e "\r\e[K\e[32m✔\e[0m All Homebrew packages are up to date!"
+            echo -e "\e[K\e[32m✔\e[0m All Homebrew packages are up to date!"
         fi
 
         echo "$current_date" > "$UPDATE_CACHE"
