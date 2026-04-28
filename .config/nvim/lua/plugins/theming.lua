15046f9ebcf583b2a6a7a45783a20c5eba2e72d2
Connor Wyatt <connor.wyatt@alpaca.markets>
2026-04-28 14:45:48 +0100

    feat(nvim): make background transparent

--- .config/nvim/lua/plugins/theming.lua
+++ .config/nvim/lua/plugins/theming.lua
@@ -5,19 +5,22 @@ return {
     },
     {
         "f-person/auto-dark-mode.nvim",
+        dependencies = {
+            "xiyaowong/transparent.nvim",
+        },
         opts = {
             update_interval = 1000,
             set_dark_mode = function()
                 local dark_mode_theme = "sakura-night"
                 vim.api.nvim_set_option_value("background", "dark", {})
                 vim.cmd("colorscheme " .. dark_mode_theme)
-                -- vim.cmd(":TransparentEnable")
+                vim.cmd(":TransparentEnable")
             end,
             set_light_mode = function()
                 local light_mode_theme = "sakura-day"
                 vim.api.nvim_set_option_value("background", "light", {})
                 vim.cmd("colorscheme " .. light_mode_theme)
-                -- vim.cmd(":TransparentEnable")
+                vim.cmd(":TransparentEnable")
             end,
         },
     },
