3ade0b0a004221ba0fee711d0556bd68103df91a
Connor Wyatt <connor.wyatt@alpaca.markets>
2026-04-28 14:43:28 +0100

    feat(zsh): add gitconfig to git source

--- .config/zsh/sources/git.sh
+++ .config/zsh/sources/git.sh
@@ -1,70 +1,85 @@
-git-worktree-add-remote() {
-    git worktree add "$1" "origin/$1"
-}
+if (( ${+commands[git]} )); then
+    git config --global core.editor nvim
+    git config --global push.autoSetupRemote true
+    git config --global init.defaultBranch main
+    git config --global commit.verbose true
+    git config --global diff.tool nvimdiff
+    git config --global difftool.prompt false
+    git config --global difftool.nvimdiff.cmd 'nvim -d $BASE $LOCAL $REMOTE $MERGED -c '\''$wincmd w'\'' -c '\''wincmd J'\'
+    git config --global merge.tool nvimdiff
+    git config --global mergetool.prompt false
+    git config --global mergetool.nvimdiff.cmd 'nvim -d $BASE $LOCAL $REMOTE $MERGED -c '\''$wincmd w'\'' -c '\''wincmd J'\'
+    git config --global alias.clone-for-worktrees '!sh $HOME/.bin/git-clone-bare-for-worktrees'
+    git config --global diff.algorithm minimal
 
-dotfiles-worktree-add-remote() {
-    dotfiles worktree add "$1" "origin/$1"
-}
+    git-worktree-add-remote() {
+        git worktree add "$1" "origin/$1"
+    }
 
-alias dg='dotfiles'
-alias dgai='dotfiles add --interactive'
-alias dgam='dotfiles am'
-alias dgap='dotfiles add --patch'
-alias dgau='dotfiles add --all --intent-to-add'
-alias dgc='dotfiles commit'
-alias dgca='dotfiles commit --amend'
-alias dgcp='dotfiles commit --patch'
-alias dgd='dotfiles diff'
-alias dgdp='dotfiles diff --patch --raw'
-alias dgdw='dotfiles diff --word-diff'
-alias dgf='dotfiles fetch --all'
-alias dgfp='dotfiles fetch --all --prune'
-alias dgl='dotfiles log'
-alias dglg="dotfiles log --graph --pretty='%C(magenta)%h%Creset%C(auto)%d%Creset %s %C(yellow)(%ad) %C(bold cyan)<%an>%Creset' --date=short"
-alias dgls='dotfiles log --stat'
-alias dglsp='dotfiles log --stat --patch'
-alias dgm='dotfiles merge'
-alias dgmtl='dotfiles mergetool --no-prompt'
-alias dgp='dotfiles push'
-alias dgpra='dotfiles pull --rebase --autostash'
-alias dgrb='dotfiles rebase'
-alias dgrs='dotfiles restore'
-alias dgrsp='dotfiles restore --patch'
-alias dgst='dotfiles status'
-alias dgsw='dotfiles switch'
-alias dgswc='dotfiles switch -c'
-alias dgwa='dotfiles worktree add'
-alias dgwar='dotfiles-worktree-add-remote'
-alias dgwl='dotfiles worktree list'
-alias dgwr='dotfiles worktree remove'
-alias g='git'
-alias gai='git add --interactive'
-alias gam='git am'
-alias gap='git add --patch'
-alias gau='git add --all --intent-to-add'
-alias gc='git commit'
-alias gca='git commit --amend'
-alias gcp='git commit --patch'
-alias gd='git diff'
-alias gdp='git diff --patch --raw'
-alias gdw='git diff --word-diff'
-alias gf='git fetch --all'
-alias gfp='git fetch --all --prune'
-alias gl='git log'
-alias glg="git log --graph --pretty='%C(magenta)%h%Creset%C(auto)%d%Creset %s %C(yellow)(%ad) %C(bold cyan)<%an>%Creset' --date=short"
-alias gls='git log --stat'
-alias glsp='git log --stat --patch'
-alias gm='git merge'
-alias gmtl='git mergetool --no-prompt'
-alias gp='git push'
-alias gpra='git pull --rebase --autostash'
-alias grb='git rebase'
-alias grs='git restore'
-alias grsp='git restore --patch'
-alias gst='git status'
-alias gsw='git switch'
-alias gswc='git switch -c'
-alias gwa='git worktree add'
-alias gwar='git-worktree-add-remote'
-alias gwl='git worktree list'
-alias gwr='git worktree remove'
+    dotfiles-worktree-add-remote() {
+        dotfiles worktree add "$1" "origin/$1"
+    }
+
+    alias dg='dotfiles'
+    alias dgai='dotfiles add --interactive'
+    alias dgam='dotfiles am'
+    alias dgap='dotfiles add --patch'
+    alias dgau='dotfiles add --all --intent-to-add'
+    alias dgc='dotfiles commit'
+    alias dgca='dotfiles commit --amend'
+    alias dgcp='dotfiles commit --patch'
+    alias dgd='dotfiles diff'
+    alias dgdp='dotfiles diff --patch --raw'
+    alias dgdw='dotfiles diff --word-diff'
+    alias dgf='dotfiles fetch --all'
+    alias dgfp='dotfiles fetch --all --prune'
+    alias dgl='dotfiles log'
+    alias dglg="dotfiles log --graph --pretty='%C(magenta)%h%Creset%C(auto)%d%Creset %s %C(yellow)(%ad) %C(bold cyan)<%an>%Creset' --date=short"
+    alias dgls='dotfiles log --stat'
+    alias dglsp='dotfiles log --stat --patch'
+    alias dgm='dotfiles merge'
+    alias dgmtl='dotfiles mergetool --no-prompt'
+    alias dgp='dotfiles push'
+    alias dgpra='dotfiles pull --rebase --autostash'
+    alias dgrb='dotfiles rebase'
+    alias dgrs='dotfiles restore'
+    alias dgrsp='dotfiles restore --patch'
+    alias dgst='dotfiles status'
+    alias dgsw='dotfiles switch'
+    alias dgswc='dotfiles switch -c'
+    alias dgwa='dotfiles worktree add'
+    alias dgwar='dotfiles-worktree-add-remote'
+    alias dgwl='dotfiles worktree list'
+    alias dgwr='dotfiles worktree remove'
+    alias g='git'
+    alias gai='git add --interactive'
+    alias gam='git am'
+    alias gap='git add --patch'
+    alias gau='git add --all --intent-to-add'
+    alias gc='git commit'
+    alias gca='git commit --amend'
+    alias gcp='git commit --patch'
+    alias gd='git diff'
+    alias gdp='git diff --patch --raw'
+    alias gdw='git diff --word-diff'
+    alias gf='git fetch --all'
+    alias gfp='git fetch --all --prune'
+    alias gl='git log'
+    alias glg="git log --graph --pretty='%C(magenta)%h%Creset%C(auto)%d%Creset %s %C(yellow)(%ad) %C(bold cyan)<%an>%Creset' --date=short"
+    alias gls='git log --stat'
+    alias glsp='git log --stat --patch'
+    alias gm='git merge'
+    alias gmtl='git mergetool --no-prompt'
+    alias gp='git push'
+    alias gpra='git pull --rebase --autostash'
+    alias grb='git rebase'
+    alias grs='git restore'
+    alias grsp='git restore --patch'
+    alias gst='git status'
+    alias gsw='git switch'
+    alias gswc='git switch -c'
+    alias gwa='git worktree add'
+    alias gwar='git-worktree-add-remote'
+    alias gwl='git worktree list'
+    alias gwr='git worktree remove'
+fi
