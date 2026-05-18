0481915d5f5a597eeb424f1bc35b794dfdf0b842
Connor Wyatt <connorwyatt1@gmail.com>
2026-05-18 12:41:31 +0100

    feat(tmux): add extra keybinds and plugins and scripts for opening tmux sessions

--- .config/zsh/sources/tmux.sh
+++ .config/zsh/sources/tmux.sh
@@ -1,6 +1,19 @@
+tmux-directory() {
+    if [[ ! -d $1 ]]; then
+        echo "Directory ($1) does not exist"
+        return
+    fi
+    tmux new -A -c $1 -s $(basename "$1" | tr . _)
+}
+
 alias ta='tmux-attacher'
+alias tbin='tmux-directory ~/.bin'
+alias tbinlocal='tmux-directory ~/.bin-local'
+alias tconfig='tmux-directory ~/.config'
+alias tdir='tmux-directory'
 alias tls='tmux ls'
 alias tnl='tmux-nvim-layout'
 alias tnlc='tmux-nvim-layout-cursor'
+alias tnvim='tmux-directory ~/.config/nvim'
 alias tpwd='tmux new -A -s $(basename "$PWD" | tr . _)'
 alias ts='tmux-sessionizer'
