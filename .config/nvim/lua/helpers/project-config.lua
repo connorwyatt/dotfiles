73322b2358b53e8e150cd9ebd0ab539c2e798075
Connor Wyatt <connor.wyatt@alpaca.markets>
2026-05-18 12:45:24 +0100

    feat(nvim): add missing project config file

---
+++ .config/nvim/lua/helpers/project-config.lua
@@ -0,0 +1,57 @@
+local M = {}
+
+local project_configuration_file_name = "project-config.lua"
+
+_G.cw.project_configuration_loaded = false
+_G.cw.project_configuration_cache = nil
+
+local function get_file_path()
+    local nvim_dir = vim.fs.find(".nvim", { upward = true, stop = vim.uv.os_homedir() })[1]
+    if not nvim_dir then
+        return nil
+    end
+
+    return vim.fs.joinpath(nvim_dir, project_configuration_file_name)
+end
+
+local function load_project_configuration()
+    if _G.cw.project_configuration_loaded then
+        return _G.cw.project_configuration_cache
+    end
+
+    local file_path = get_file_path()
+
+    if file_path == nil then
+        return _G.cw.project_configuration_cache
+    end
+
+    if vim.loop.fs_stat(file_path) then
+        local ok, content = pcall(vim.secure.read, file_path)
+        if ok and content then
+            _G.cw.project_configuration_loaded = true
+            _G.cw.project_configuration_cache = loadstring(content)()
+        end
+    end
+
+    return _G.cw.project_configuration_cache
+end
+
+local function execute_project_configuration_hook(hook_name)
+    local project_configuration = load_project_configuration()
+
+    if project_configuration == nil or project_configuration[hook_name] == nil then
+        return
+    end
+
+    project_configuration[hook_name]()
+end
+
+function M.execute_init_hook()
+    execute_project_configuration_hook("init")
+end
+
+function M.execute_loaded_hook()
+    execute_project_configuration_hook("loaded")
+end
+
+return M
