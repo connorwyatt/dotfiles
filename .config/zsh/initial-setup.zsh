e6db1eb53b65851e999d3a6fae7ee807f739303a
Connor Wyatt <connorwyatt1@gmail.com>
2026-05-26 21:20:05 +0100

    feat(zsh): only run brew bundle when file hash changes

--- .config/zsh/initial-setup.zsh
+++ .config/zsh/initial-setup.zsh
@@ -1,3 +1,10 @@
 if (( ${+commands[brew]} )); then
-    brew bundle --file="$HOME/.config/zsh/Brewfile" --no-upgrade --quiet
+    local bundle_cache="$HOME/.config/zsh/.brew_bundle"
+    local current_date=$(date +%Y%m%d)
+
+    if [[ ! -f "$bundle_cache" ]] || [[ "$(cat "$bundle_cache")" != "$current_date" ]]; then
+        brew bundle --file="$HOME/.config/zsh/Brewfile" --no-upgrade --quiet
+
+        echo "$current_date" > "$bundle_cache"
+    fi
 fi
