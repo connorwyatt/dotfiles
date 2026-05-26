5c11c0f93630fa97836970031edfcade119d06c2
Connor Wyatt <connorwyatt1@gmail.com>
2026-05-26 21:21:10 +0100

    feat(nvim): turn off transparency

--- .config/nvim/lua/plugins/theming.lua
+++ .config/nvim/lua/plugins/theming.lua
@@ -14,13 +14,13 @@ return {
                 local dark_mode_theme = "sakura-night"
                 vim.api.nvim_set_option_value("background", "dark", {})
                 vim.cmd("colorscheme " .. dark_mode_theme)
-                vim.cmd(":TransparentEnable")
+                -- vim.cmd(":TransparentEnable")
             end,
             set_light_mode = function()
                 local light_mode_theme = "sakura-day"
                 vim.api.nvim_set_option_value("background", "light", {})
                 vim.cmd("colorscheme " .. light_mode_theme)
-                vim.cmd(":TransparentEnable")
+                -- vim.cmd(":TransparentEnable")
             end,
         },
     },
