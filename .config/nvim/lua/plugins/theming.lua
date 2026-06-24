a3e16a0dde567ca9df2fb948ad24726574bb14e0
Connor Wyatt <connorwyatt1@gmail.com>
2026-06-24 10:11:15 +0100

    feat(nvim): use themes.nvim package

--- .config/nvim/lua/plugins/theming.lua
+++ .config/nvim/lua/plugins/theming.lua
@@ -2,32 +2,8 @@ local themes = require("helpers.themes")
 
 return {
     {
-        "connorwyatt/sakura.nvim",
+        "connorwyatt/themes.nvim",
         priority = 10000,
-    },
-
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        priority = 10000,
-        opts = {
-            highlight_groups = {
-                StatusLineModeCommand = { bg = "foam", fg = "overlay", bold = true },
-                StatusLineModeCommandText = { bg = "NONE", fg = "foam", bold = true },
-                StatusLineModeInsert = { bg = "gold", fg = "overlay", bold = true },
-                StatusLineModeInsertText = { bg = "NONE", fg = "gold", bold = true },
-                StatusLineModeNormal = { bg = "rose", fg = "overlay", bold = true },
-                StatusLineModeNormalText = { bg = "NONE", fg = "rose", bold = true },
-                StatusLineModeReplace = { bg = "love", fg = "overlay", bold = true },
-                StatusLineModeReplaceText = { bg = "NONE", fg = "love", bold = true },
-                StatusLineModeSelect = { bg = "love", fg = "overlay", bold = true },
-                StatusLineModeSelectText = { bg = "NONE", fg = "love", bold = true },
-                StatusLineModeTerminal = { bg = "iris", fg = "overlay", bold = true },
-                StatusLineModeTerminalText = { bg = "NONE", fg = "iris", bold = true },
-                StatusLineModeVisual = { bg = "pine", fg = "overlay", bold = true },
-                StatusLineModeVisualText = { bg = "NONE", fg = "pine", bold = true },
-            },
-        },
     },
 
     {
@@ -36,7 +12,7 @@ return {
             "xiyaowong/transparent.nvim",
         },
         opts = {
-            update_interval = 1000,
+            update_interval = 300000,
             set_dark_mode = function()
                 local dark_mode_theme = "Sakura Night"
                 vim.api.nvim_set_option_value("background", "dark", {})
