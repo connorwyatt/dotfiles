71d9a33889c1b41f5b48b18878932839fc8fad0a
Connor Wyatt <connorwyatt1@gmail.com>
2026-06-26 12:31:05 +0100

    feat(nvim): remove all telescope related commands added by text-case

--- .config/nvim/after/plugin/text-case.lua
+++ .config/nvim/after/plugin/text-case.lua
@@ -1,3 +1,24 @@
+local telescope_commands = {
+    "TextCaseOpenTelescope",
+    "TextCaseOpenTelescopeQuickChange",
+    "TextCaseOpenTelescopeLSPChange",
+}
+
+local function remove_telescope_commands()
+    for _, name in ipairs(telescope_commands) do
+        pcall(vim.api.nvim_del_user_command, name)
+    end
+end
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "LazyLoad",
+    callback = function(event)
+        if event.data == "text-case.nvim" then
+            vim.schedule(remove_telescope_commands)
+        end
+    end,
+})
+
 local commands = {
     TextCaseSnake = "to_snake_case",
     TextCaseCamel = "to_camel_case",
