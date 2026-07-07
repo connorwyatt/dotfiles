550e417ffce9bbc9e212e088d88b2ebbbbbad650
Connor Wyatt <connor.wyatt@alpaca.markets>
2026-07-07 22:17:24 +0100

    feat(zsh): update tmux-sessionizer

--- .config/zsh/sources/tmux.zsh
+++ .config/zsh/sources/tmux.zsh
@@ -8,34 +8,28 @@ tmux-attach() {
 }
 
 tmux-create-or-attach() {
-    session_name="${${2:-${1:t}}//./_}"
+    local dir=${~1}
 
-    if [[ -z $TMUX ]]; then
-        tmux new-session -A -s $session_name -c $1
+    if [[ ! -d $dir ]]; then
+        echo "Directory ($1) does not exist"
         return
     fi
 
-    tmux new-session -ds $session_name -c $1
+    session_name="${${2:-${1}}//./_}"
+
+    if [[ -z $TMUX ]]; then
+        tmux new-session -A -s $session_name -c $dir
+        return
+    fi
+
+    tmux new-session -ds $session_name -c $dir
 
     tmux switch-client -t $session_name
 }
 
-tmux-directory() {
-    if [[ ! -d $1 ]]; then
-        echo "Directory ($1) does not exist"
-        return
-    fi
-    tmux-create-or-attach "$1" "$2"
-}
-
 alias ta='tmux-attacher'
-alias tbin='tmux-directory ~/.bin'
-alias tbinlocal='tmux-directory ~/.bin-local'
-alias tconfig='tmux-directory ~/.config'
-alias tdir='tmux-directory'
 alias tls='tmux ls'
 alias tnl='tmux-nvim-layout'
 alias tnlc='tmux-nvim-layout-cursor'
-alias tnvim='tmux-directory ~/.config/nvim'
 alias tpwd='tmux-create-or-attach $PWD'
 alias ts='tmux-sessionizer'
