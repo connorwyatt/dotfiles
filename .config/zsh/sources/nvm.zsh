6ce1c78bbf43a2b466ecdd573ccf332ad272f85a
Connor Wyatt <wyatt.connor@proton.me>
2026-07-30 23:39:33 +0100

    feat(nvm): ensure that paths are correct

--- .config/zsh/sources/nvm.zsh
+++ .config/zsh/sources/nvm.zsh
@@ -1,7 +1,6 @@
 if brew list --formula | grep -q "^nvm$"; then
     export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-    path+=($NVM_DIR)
+    local nvm_prefix="$(brew --prefix nvm)"
+    [ -s "$nvm_prefix/nvm.sh" ] && \. "$nvm_prefix/nvm.sh"
+    [ -s "$nvm_prefix/etc/bash_completion.d/nvm" ] && \. "$nvm_prefix/etc/bash_completion.d/nvm"
 fi
