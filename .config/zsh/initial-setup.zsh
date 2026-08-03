6169ac51374494205ad8f7016077923960f8a2f9
Connor Wyatt <wyatt.connor@proton.me>
2026-08-03 22:27:25 +0100

    feat(homebrew): only update homebrew weekly instead of daily

--- .config/zsh/initial-setup.zsh
+++ .config/zsh/initial-setup.zsh
@@ -1,10 +1,10 @@
 if (( ${+commands[brew]} )); then
     local bundle_cache="$HOME/.config/zsh/.brew_bundle"
-    local current_date=$(date +%Y%m%d)
+    local current_week=$(date +%Y%V)
 
-    if [[ ! -f "$bundle_cache" ]] || [[ "$(cat "$bundle_cache")" != "$current_date" ]]; then
+    if [[ ! -f "$bundle_cache" ]] || [[ "$(cat "$bundle_cache")" != "$current_week" ]]; then
         brew bundle --file="$HOME/.config/zsh/Brewfile" --no-upgrade --quiet
 
-        echo "$current_date" > "$bundle_cache"
+        echo "$current_week" > "$bundle_cache"
     fi
 fi
--- .config/zsh/sources/homebrew.zsh
+++ .config/zsh/sources/homebrew.zsh
@@ -5,12 +5,10 @@ if (( ${+commands[brew]} )); then
     export HOMEBREW_NO_AUTO_UPDATE=1
 
     local update_cache="$HOME/.config/zsh/.brew_last_check"
-    local current_date=$(date +%Y%m%d)
+    local current_week=$(date +%Y%V)
 
-    if [[ ! -f "$update_cache" ]] || [[ "$(cat "$update_cache")" != "$current_date" ]]; then
-        brew update
-
-        echo "\nChecking for Homebrew updates..."
+    if [[ ! -f "$update_cache" ]] || [[ "$(cat "$update_cache")" != "$current_week" ]]; then
+        brew update-if-needed
 
         data=$(brew outdated --json=v2)
 
@@ -18,13 +16,11 @@ if (( ${+commands[brew]} )); then
         casks_list=$(echo "$data" | jq -r '[.casks[].name] | join(", ")')
 
         if [[ -n "$formulae_list" || -n "$casks_list" ]]; then
-            echo -e "\e[KOutdated Homebrew packages:"
+            echo -e "Outdated Homebrew packages:"
             [[ -n "$formulae_list" ]] && echo -e "  \e[33m•\e[0m Formulae: $formulae_list"
             [[ -n "$casks_list" ]] && echo -e "  \e[33m•\e[0m Casks: $casks_list"
-        else
-            echo -e "\e[K\e[32m✔\e[0m All Homebrew packages are up to date!"
         fi
 
-        echo "$current_date" > "$update_cache"
+        echo "$current_week" > "$update_cache"
     fi
 fi
